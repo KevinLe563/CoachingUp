fbede29f29be425a7fcf7fc14b1ff6b5a0d5b117
Kevin Le <k26le@uwaterloo.ca>
2023-02-01 23:22:03 -0500

    add placeholder nav

--- coaching-up/src/Shared/components/Navigation/MainNavigation.tsx
+++ coaching-up/src/Shared/components/Navigation/MainNavigation.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Button } from 'react-bootstrap';
+import { Button, Nav, NavDropdown } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 import './MainNavigation.css';
 
 
 function MainNavigation() {
     return (
-        <Navbar>
-            <Container>
-                <Navbar.Brand>Find-Coaching</Navbar.Brand>
+        <Navbar expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="">Find Coaching</Navbar.Brand>
+                <Navbar.Toggle aria-controls='navbarScroll' />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        navbarScroll
+                    >
+                        <Nav.Link href="">Link</Nav.Link>
+                        <Nav.Link href="">Link</Nav.Link>
+                        <NavDropdown title="Profile" id="navbarScrollingDropdown">
+                            <NavDropdown.Item hreef="">1</NavDropdown.Item>
+                            <NavDropdown.Item hreef="">1</NavDropdown.Item>
+                            <NavDropdown.Item hreef="">1</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item hreef="">Sign Out</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control 
+                            type="search"
+                            placeholder="search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )

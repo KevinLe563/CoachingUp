ceb144b65d07de4d2cb7200829c019464aec39e2
k26le <k26le@uwaterloo.ca>
2023-02-05 23:51:19 -0500

    add form elements

--- coaching-up/src/User/components/PostingForm.tsx
+++ coaching-up/src/User/components/PostingForm.tsx
@@ -8,8 +8,11 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 
+import './PostingForm.css';
 import { UserInfo } from "../../Types/UserTypes";
 import { CoachInfo } from "../../Types/CoachTypes";
+import { Container, FormControl } from "react-bootstrap";
+import { ListingTags } from "../../Types/EnumTypes";
 
 const schema = Yup.object().shape({
     title: Yup.string().required(),
@@ -20,10 +23,91 @@ const schema = Yup.object().shape({
     terms: Yup.bool().required().oneOf([true], "Terms must be accepted"),
 });
 
+function GenerateMethodOptions() {
+    return (
+        (Object.keys(ListingTags) as Array<keyof typeof ListingTags>).map((key) => {
+            return (
+                <option>
+                    {key}
+                </option>
+            )
+        })
+    );
+}
+
 function PostingForm(props: CoachInfo) {
     return (
         <>
-        
+            <Container className="form-container border">
+                <Form className="form">
+                    <h1>New Posting</h1>
+
+                    <Form.Group className="form-group">
+                        <Row>
+                            <Col>
+                                <Form.Label>Coach First Name</Form.Label>
+                                <Form.Control placeholder={`${props.coachFirstName}`} disabled />
+                            </Col>
+                            <Col>
+                                <Form.Label>Coach Last Name</Form.Label>
+                                <Form.Control placeholder={`${props.coachLastName}`} disabled />
+                            </Col>
+                        </Row>
+                    </Form.Group>
+
+                    <Form.Group className="form-group">
+                        <Row>
+                            <Col>
+                            <Form.Label>
+                                Title
+                            </Form.Label>
+                            <Form.Control 
+                                required
+                            />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Label>
+                                    Description
+                                </Form.Label>
+                                <Form.Control
+                                    as="textarea" 
+                                    aria-label="Description"
+                                    required
+                                />
+                            </Col>
+                        </Row>
+                    </Form.Group>
+
+                    <Form.Group className="form-group">
+                        <Row>
+                            <Col>
+                                <Form.Label>Upload Photo</Form.Label>
+                                <Form.Control 
+                                    type="file"
+                                    name="file"
+                                />
+                            </Col>
+                        </Row>
+                    </Form.Group>
+
+                    <Form.Group className="form-group">
+                        <Row>
+                            <Col>
+                                <Form.Label>
+                                    Method
+                                </Form.Label>
+                                <Form.Control as="select">
+                                    <>
+                                        {GenerateMethodOptions()}
+                                    </>
+                                </Form.Control>
+                            </Col>
+                        </Row>
+                    </Form.Group>
+                </Form>
+            </Container>
         </>
     );
 }

b29cdc076c90560866838f068c9011aa659cd153
Kevin Le <k26le@uwaterloo.ca>
2023-03-05 18:09:08 -0500

    fix tsconfig path and scripts

--- coaching-up-backend/routes/listing-routes.ts
+++ coaching-up-backend/routes/listing-routes.ts
@@ -5,8 +5,11 @@ import { user1, LISTINGS } from '@frontend/Testing/Constants/Constants';
 const listingRouter: Router = express.Router();
 
 listingRouter.get('/:lid', (req, res, next) => {
-    console.log('GET Request in listing');
-    res.json({message: 'Working'});
+    const listingId = Number(req.params.lid);
+    const listing = LISTINGS.listings.find(l => {
+        return l.listing_id === listingId;
+    })
+    res.json(listing);
 });
 
 export default listingRouter;
